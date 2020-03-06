9176842fe1d20d2e4afb7ed90ff813e83ba7cf89
Your Name <you@example.com>
2020-03-05 18:57:04 -0600

    edit3

--- rolling.js
+++ rolling.js
@@ -5,7 +5,7 @@
 let result = ''
 
 document.getElementById('button').onclick = function() {
-    let n = document.getElementById("number").Value
+    let n = document.getElementById("number").value
     let resultOne
     let resultTwo
     for (let i = 0; i <= n; i++){
@@ -13,10 +13,7 @@ document.getElementById('button').onclick = function() {
     max = Math.floor(7);
     resultOne = Math.floor(Math.random() * (max - min)) + min;
     resultTwo = Math.floor(Math.random() * (max - min)) + min;
-    result += resultOne + resultTwo
+    result = resultOne + resultTwo
     }
-    
-    return result
-  }
-var t = document.createTextNode(result + '');
-document.getElementById("output").innerHTML = t 
+    return (document.getElementById("output").textContent = result )
+}
